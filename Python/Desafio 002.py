3484a21d901e9e60040fa8e8892ae7b19fb65711
RiderBR <59567130+RiderBR@users.noreply.github.com>
2020-05-13 00:57:35 -0300

    Defaios da aula de python

---
+++ Python/Desafio 002.py
@@ -0,0 +1,4 @@
+dia = int(input("Qual o dia que você nasceu? "))
+mes = str(input("Qual o mês de seu nascimento? "))
+ano = int(input("Qual o ano de seu nascimento? "))
+print("Você nasceu em {} de {} de {}. Estou correto?".format(dia, mes, ano))
